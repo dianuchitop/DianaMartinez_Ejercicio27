8e894c7e8c4955baab5487bb7290a54fc9bf9fe0
dianuchitop <48950463+dianuchitop@users.noreply.github.com>
2019-04-30 07:33:49 -0500

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -1,15 +1,16 @@
 import networkx as nx                                                                                                                        
 import matplotlib.pyplot as plt                                                 
 
-#n=100 Number of nodes                                                          
-ncols=10 #Number of columns in a 10x10 grid of positions                        
+                                                       
+ncols=6
 
-N=10 #Nodes per side                                                            
+N=7 #Nodes per side                                                            
 G=nx.grid_2d_graph(N,N)                                                         
 pos = dict(zip(G.nodes(),G.nodes()))                                            
 ordering = [(y,N-1-x) for y in range(N) for x in range(N)]                      
 labels = dict(zip(ordering, range(len(ordering))))                              
-nx.draw_networkx(G, pos=pos, with_labels=False, node_size = 250, node_color='lightblue')                                                                       
+nx.draw_networkx(G, pos=pos, with_labels=False, node_size = 300, node_color='green')                                                                       
 nx.draw_networkx_labels(G, pos=pos, labels=labels)                              
 plt.axis('off')                                                                 
 plt.show()
+plt.savefig("ldjnd")
